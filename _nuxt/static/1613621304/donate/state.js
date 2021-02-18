7af990246defffd38961415fd350a5ff2789fd0e
W1lll <w1ll.fost3r@gmail.com>
2021-02-18 04:08:38 +0000

    Updates

---
+++ _nuxt/static/1613621304/donate/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fthebenjifoundation\u002F_nuxt\u002Fstatic\u002F1613621304",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdonate",config:{app:{basePath:"\u002Fthebenjifoundation\u002F",assetsPath:"\u002Fthebenjifoundation\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"68d4341c"}}}}(null));
